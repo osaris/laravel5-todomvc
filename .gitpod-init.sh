2938c0b7ad0bcea5b4d6db0acad514b79e175245
osaris <raphael.emourgeon@gmail.com>
2020-09-08 22:13:00 +0000

    Add gitpod configuration

---
+++ .gitpod-init.sh
@@ -0,0 +1,6 @@
+mysql -u root -e "create database laravel5-todomvc"
+cp .env.example .env
+sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
+sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
+composer install
+php artisan key:generate
